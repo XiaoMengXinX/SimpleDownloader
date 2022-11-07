508ce45dabbdb754407627b3890474e8d352517c
XiaoMengXinX <xibaole2333@gmail.com>
2022-11-08 01:53:26 +0800

    fix

--- downloader.go
+++ downloader.go
@@ -71,6 +71,7 @@ func (d *DownloadTask) Download() (err error) {
 		ranges = [][]int64{{0, d.fileSize - 1}}
 		threads = 1
 	}
+	d.writtenBytes = 0
 
 	ctx, cancel := context.WithTimeout(context.Background(), d.Downloader.TimeOut)
 
